3d80f683b464f3ed5a88f1a4c19c11b296ae6cce
GreySou1 <prudvi.kamtam16@gmail.com>
2020-05-29 13:56:16 +0530

    Calculates the statistics of the csv files

--- csvanalysis.py
+++ csvanalysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-#data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/AvgStd_metrics.csv')
+f = open('statistics.txt', 'w', encoding = 'utf-8')
 def query(a, b):
     '''
     returns number of files where columns a is greater than column b
@@ -10,39 +10,88 @@ def query(a, b):
 
 metric_list = ['FRE', 'SI', 'FKG', 'CLI', 'ARI', 'DCRS', 'DW', 'LWF', 'GF']
 counts_list = ['Words', 'Wikilinks', 'References']
-frames = ['BR', 'AR'] # Before Release, After Release
+rankframes = ['BR', 'AR'] # Before Release, After Release
+avgstdframes = ['120 frame', '60 frame']
 
-'''
-for item in metric_list:
-    for frame in frames:
-        print('Number of articles where average of '+item+' in '+frame+' is greater than all revisions = ' + str(query('Avg '+item+' of '+frame,'Avg '+item+' of all Revi')))
 
-for item in metric_list:
-    for frame in frames:
-        print('Number of articles where STD of '+item+' in '+frame+' is greater than all revisions = ' + str(query('STD '+item+' of '+frame,'Avg '+item+' of all Revi')))
-'''
-print('=============== COUNTS ===============')
+
+f.write('==================== COUNTS ====================')
+f.write('\n')
+# printing the counts's average and std statistics
+data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/AvgStd_counts.csv')
+f.write('\nNUMBER OF ARTICLES (TOTAL = '+str(data.shape()[0])+') WHERE AVERAGE OF: \n')
+f.write('\n')
+for item in counts_list:
+    for frame in avgstdframes:
+        f.write(item+' in '+frame+' is greater than all revisions = ' + str(query('Avg '+item+' of '+frame,'Avg '+item+' of all Revi'))) # should be high
+        f.write('\n')
+
+f.write('\nNUMBER OF ARTICLES (TOTAL = '+str(data.shape()[0])+') WHERE STD OF: \n')
+f.write('\n')
+for item in counts_list:
+    for frame in avgstdframes:
+        f.write(item+' in '+frame+' is greater than all revisions = ' + str(query('STD '+item+' of '+frame,'Avg '+item+' of all Revi'))) # should be low
+        f.write('\n')
+
+# printing the counts's 60 and 120 frames's slope window ranks statistics
 data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/slopewindowrank_counts60.csv')
 
-print('\nFOR WINDOW SIZE OF 60 NUMBER OF FILES WHERE :\n')
+f.write('\nFOR WINDOW SIZE OF 60 NUMBER OF FILES (TOTAL = '+str(data.shape()[0])+') WHERE :\n')
+f.write('\n')
 for item in counts_list:
-    print(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR')))
+    f.write(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR'))) # should be high
+    f.write('\n')
 
 data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/slopewindowrank_counts120.csv')
 
-print('\nFOR WINDOW SIZE OF 120 NUMBER OF FILES WHERE :\n')
+f.write('\nFOR WINDOW SIZE OF 120 NUMBER OF FILES (TOTAL = '+str(data.shape()[0])+') WHERE :\n')
+f.write('\n')
 for item in counts_list:
-    print(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR')))
+    f.write(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR'))) # should be high
+    f.write('\n')
 
-print('\n=============== METRICS ===============')
+f.write('\n==================== METRICS ====================')
+f.write('\n')
+# printing the metrics's average and std statistics
+data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/AvgStd_metrics.csv')
+f.write('\nNUMBER OF ARTICLES (TOTAL = '+str(data.shape()[0])+') WHERE AVERAGE OF: \n')
+f.write('\n')
+for item in metric_list:
+    for frame in avgstdframes:
+        f.write(item+' in '+frame+' is greater than all revisions = ' + str(query('Avg '+item+' of '+frame,'Avg '+item+' of all Revi'))) # should be high
+        f.write('\n')
+
+f.write('\nNUMBER OF ARTICLES (TOTAL = '+str(data.shape()[0])+') WHERE STD OF: \n')
+f.write('\n')
+for item in metric_list:
+    for frame in avgstdframes:
+        f.write(item+' in '+frame+' is greater than all revisions = ' + str(query('STD '+item+' of '+frame,'Avg '+item+' of all Revi'))) # should be low
+        f.write('\n')
+
+# printing the metrics's 60 and 120 frames's slope window ranks statistics
 data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/slopewindowrank_metrics60.csv')
 
-print('\nFOR WINDOW SIZE OF 60 NUMBER OF FILES WHERE :\n')
+f.write('\nFOR WINDOW SIZE OF 60 NUMBER OF FILES (TOTAL = '+str(data.shape()[0])+') WHERE :\n')
+f.write('\n')
 for item in metric_list:
-    print(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR')))
-
+    f.write(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR'))) # should be high
+    f.write('\n')
 
 data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/slopewindowrank_metrics120.csv')
-print('\nFOR WINDOW SIZE OF 120 NUMBER OF FILES WHERE :\n')
+f.write('\nFOR WINDOW SIZE OF 120 NUMBER OF FILES (TOTAL = '+str(data.shape()[0])+') WHERE :\n')
+f.write('\n')
 for item in metric_list:
-    print(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR')))
+    f.write(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR'))) # should be high
+    f.write('\n')
+
+f.close()
+'''
+data = pd.read_csv('/Users/prudvikamtam/Projects/BiopicAnalysis/AvgStd_ores.csv')
+ores_list = ['damaging', 'goodfaith']
+# for item in ores_list:
+#     f.write(item + ' rank BR is greater than '+item+' rank AR = ', str(query(item+' Rank BR', item+' Rank AR')))
+        f.write('\n')
+
+f.write('Avg goodfaith of 120 frame is less than all revi = ' + str(len(np.where(data['Avg goodfaith of all Revi'] > data['Avg goodfaith of 120 frame'])[0])))
+f.write('\n')
+'''
